fccbbd6ee31b5949ab52e9c53f7a1e1b06d84716
Viacheslav Poturaev <vearutop@users.noreply.github.com>
2020-01-26 21:52:06 +0100

    Fix PHP version check (#30)

--- src/JsonPointer.php
+++ src/JsonPointer.php
@@ -104,7 +104,7 @@ class JsonPointer
         $ref = &$holder;
         while (null !== $key = array_shift($pathItems)) {
             if ($ref instanceof \stdClass || is_object($ref)) {
-                if (PHP_VERSION_ID < 71000 && '' === $key) {
+                if (PHP_VERSION_ID < 70100 && '' === $key) {
                     throw new Exception('Empty property name is not supported by PHP <7.1',
                         Exception::EMPTY_PROPERTY_NAME_UNSUPPORTED);
                 }
@@ -202,7 +202,7 @@ class JsonPointer
         $ref = $holder;
         while (null !== $key = array_shift($pathItems)) {
             if ($ref instanceof \stdClass) {
-                if (PHP_VERSION_ID < 71000 && '' === $key) {
+                if (PHP_VERSION_ID < 70100 && '' === $key) {
                     throw new Exception('Empty property name is not supported by PHP <7.1',
                         Exception::EMPTY_PROPERTY_NAME_UNSUPPORTED);
                 }
