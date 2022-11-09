swaggest/json-diff

commit 2b56e5d516ad5301fefd49641163736ceb0ae706

Postpone exception throwing

src/JsonPatch.php
@@ -70,13 +70,6 @@ class JsonPatch implements \JsonSerializable
                 throw new MissingFieldException('path', $operation);
             }
 
-            if (!is_string($operation->op)) {
-                throw new InvalidFieldTypeException('op', 'string', $operation);
-            }
-            if (!is_string($operation->path)) {
-                throw new InvalidFieldTypeException('path', 'string', $operation);
-            }
-
             $op = null;
             switch ($operation->op) {
                 case Add::OP:
@@ -98,8 +91,17 @@ class JsonPatch implements \JsonSerializable
                     $op = new Test();
                     break;
                 default:
+                    if (!is_string($operation->op)) {
+                        throw new InvalidFieldTypeException('op', 'string', $operation);
+                    }
+
                     throw new UnknownOperationException($operation);
             }
+
+            if (!is_string($operation->path)) {
+                throw new InvalidFieldTypeException('path', 'string', $operation);
+            }
+            
             $op->path = $operation->path;
             if ($op instanceof OpPathValue) {
                 if (property_exists($operation, 'value')) {

commit 191ea3ec2b2da23850bd4a4b1b56f00dd55c2863

Fix error if "op" is boolean true
Reverts the change in 2b56e5d516ad5301fefd49641163736ceb0ae706 which introduced a bug if "op" is boolean true.

Amends test to prevent future regression.

src/JsonPatch.php
@@ -70,6 +70,13 @@ class JsonPatch implements \JsonSerializable
                 throw new MissingFieldException('path', $operation);
             }
 
+            if (!is_string($operation->op)) {
+                throw new InvalidFieldTypeException('op', 'string', $operation);
+            }
+            if (!is_string($operation->path)) {
+                throw new InvalidFieldTypeException('path', 'string', $operation);
+            }
+
             $op = null;
             switch ($operation->op) {
                 case Add::OP:
@@ -91,17 +98,8 @@ class JsonPatch implements \JsonSerializable
                     $op = new Test();
                     break;
                 default:
-                    if (!is_string($operation->op)) {
-                        throw new InvalidFieldTypeException('op', 'string', $operation);
-                    }
-
                     throw new UnknownOperationException($operation);
             }
-
-            if (!is_string($operation->path)) {
-                throw new InvalidFieldTypeException('path', 'string', $operation);
-            }
-            
             $op->path = $operation->path;
             if ($op instanceof OpPathValue) {
                 if (property_exists($operation, 'value')) {

tests/src/JsonPatchTest.php
@@ -137,7 +137,7 @@ JSON;
     {
         return [
             '"op" invalid type' => [
-                (object)array('op' => array('foo' => 'bar'), 'path' => '/123', 'value' => 'test'),
+                (object)array('op' => true, 'path' => '/123', 'value' => 'test'),
                 'Invalid field type - "op" should be of type: string',
                 'op',
                 'string'